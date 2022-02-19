9f98417238a596ace5538d003ccfc505986e1787
Walid Hocine <walid.hocine.algerie@viacesi.fr>
2022-02-20 00:23:19 +0100

    push terraform code

---
+++ main.tf
@@ -0,0 +1,68 @@
+terraform {
+   required_providers {
+     azurerm = {
+       source = "hashicorp/azurerm"
+       version = "2.97.0"
+     }
+
+     
+   }
+
+
+   required_version = ">= 1.1.0"
+ }
+ provider "azurerm" {
+
+     features {
+       
+     }
+ }
+ resource "azurerm_resource_group" "rg" {
+   name     = "terraform-multiplevm-poc"
+   location = "westus"
+ }
+ resource "azurerm_virtual_network" "rg" {
+   name                = "rg-network"
+   address_space       = ["10.0.0.0/16"]
+   location            = azurerm_resource_group.rg.location
+   resource_group_name = azurerm_resource_group.rg.name
+ }
+ resource "azurerm_subnet" "rg" {
+   name                 = "internal"
+   resource_group_name  = azurerm_resource_group.rg.name
+   virtual_network_name = azurerm_virtual_network.rg.name
+   address_prefixes     = ["10.0.3.0/24"]
+ }
+ resource "azurerm_network_interface" "rg" {
+   count = 2  
+   name                = "AzVM-NIC-${count.index}"
+   location            = azurerm_resource_group.rg.location
+   resource_group_name = azurerm_resource_group.rg.name
+ ip_configuration {
+     name                          = "internal"
+     subnet_id                     = azurerm_subnet.rg.id
+     private_ip_address_allocation = "Dynamic"
+   }
+ }
+ resource "azurerm_windows_virtual_machine" "rg" {
+   count = 2  
+   name                = "AzVM-${count.index}"
+   resource_group_name = azurerm_resource_group.rg.name
+   location            = azurerm_resource_group.rg.location
+   size                = "Standard_B2ms"
+   admin_username      = "adminusername"
+   admin_password      = "123@abc@superpass"
+   network_interface_ids = [
+     azurerm_network_interface.rg.*.id[count.index],
+   ]
+ os_disk {
+     caching              = "ReadWrite"
+     storage_account_type = "Standard_LRS"
+   }
+ source_image_reference {
+     publisher = "MicrosoftWindowsServer"
+     offer     = "WindowsServer"
+     sku       = "2022-Datacenter"
+     version   = "latest"
+   }
+ }
